0b64e2b4be9a83fa90d5a02c30e232d4cdbd3a75
zukaishi <ishizuka215@gmail.com>
2021-12-29 08:12:13 +0900

    update

---
+++ zukaishi-hello-world.user.js
@@ -0,0 +1,16 @@
+// ==UserScript==
+// @name         zukaishi-hello-world
+// @namespace    zukaishi-hello-world
+// @version      0.1
+// @description  try to take over the world!
+// @author       zukaishi
+// @match        https://github.com/zukaishi
+// @icon         https://www.google.com/s2/favicons?domain=github.com
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+   console.log("hello,world")
+   alert("hello,world")
+})();
