bdf6164f142007d4fcdcd9c6473a4f3326237428
zukaishi <ishizuka215@gmail.com>
2022-01-02 11:14:22 +0900

    update

--- google-link-change.js
+++ google-link-change.js
@@ -1,18 +1,27 @@
 // ==UserScript==
-// @name         zukaishi-hello-world
-// @namespace    zukaishi-hello-world
+// @name         zukaishi-google-link-change
+// @namespace    zukaishi-google-link-change
 // @version      0.1
 // @description  try to take over the world!
 // @author       zukaishi
 // @match        https://github.com/zukaishi
 // @icon         https://www.google.com/s2/favicons?domain=github.com
 // @grant        none
+// @require  http://ajax.googleapis.com/ajax/libs/jquery/2.1.0/jquery.min.js
+// @match    https://*.google.com/*
 // ==/UserScript==
-/**
- * Todo input class is gNO89b of value = Google 検索-> Google 検索 test change
- */
+
 (function() {
     'use strict';
-   console.log("hello,world")
-   alert("hello,world")
+   console.log("google-link-change")
+
+        alert("google-link-change1")
+
+    var targNode    = document.getElementById ("gNO89b");
+        alert("google-link-change2")
+    targNode.classList.remove ("disabled");
+    alert("google-link-change4")
+    targNode.classList.add ("enabled");
+
+    alert("google-link-change3")
 })();
